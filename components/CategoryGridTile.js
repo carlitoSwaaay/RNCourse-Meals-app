863b653c0330c0a4024cd8b13adcf5b892995491
carlitoSwaaay <cacollier02@yahoo.com>
2023-06-03 17:06:00 -0700

    Update CategoryGridTile.js

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -3,7 +3,12 @@ import { View, Pressable, Text, StyleSheet, Platform } from "react-native";
 function CategoryGridTile({ title, color }) {
   return (
     <View style={styles.gridItem}>
-      <Pressable android_ripple={{ color: '#ccc' }} style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]}>
+      <Pressable
+        android_ripple={{ color: '#ccc' }}
+        style={({ pressed }) => [
+          styles.button, pressed ? styles.buttonPressed : null,
+        ]}
+      >
         <View style={[styles.innerContainer, { backgroundColor: color }]}>
           <Text style={styles.title}>{title}</Text>
       </View>
