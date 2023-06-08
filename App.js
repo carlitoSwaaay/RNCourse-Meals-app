b28e4d7decebc6883bbc75f2e8f54db5bc5985de
carlitoSwaaay <cacollier02@yahoo.com>
2023-06-07 17:39:37 -0700

    corrected typo

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
-          <Stack.Screen name="MealsOVerview" component={MealsOverviewScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
