1e7935565faac957c4f1a9b9463759d12b943ddf
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-18 14:14:48 -0700

    Create mealItem.js

---
+++ components/mealItem.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const MealItem = ({ title }) => {
+  <View>
+    <Text>{title}</Text>
+  </View>
+};
+
+export default MealItem;
+
+const styles = StyleSheet.create({
+
+});
