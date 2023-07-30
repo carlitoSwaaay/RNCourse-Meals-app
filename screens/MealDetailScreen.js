737d87f48137abdaacf8321aabccc3f58c4b98b1
carlitoSwaaay <cacollier02@yahoo.com>
2023-07-30 14:11:40 -0700

    Create MealDetailScreen.js

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MealDetailScreen = () => {
+  return (
+    <View>
+      <Text>MealDetailScreen</Text>
+    </View>
+  )
+}
+
+export default MealDetailScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
